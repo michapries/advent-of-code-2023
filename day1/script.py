98f68b87724482983f3d2d2f162fe17d9d306133
Micha-Alexander Pries <micha-alexander.pries@rwth-aachen.de>
2023-12-01 19:40:24 +0100

    finished day 1 and updated template

--- day1/script.py
+++ day1/script.py
@@ -1,15 +1,54 @@
 import numpy as np
 import pandas as pd
+from datetime import date
+from num2words import num2words
+from words2num import w2n
 
-def task_one():
-    return ""
+def get_input():
 
+    day = date.today().strftime('%-d')
 
-def task_two():
-    return ""
+    with open(f'./day{day}/input.txt', 'r') as input:
+        return input.readlines()
 
+# include_words is a boolean that is used to include the words for each digit (task 2)
+def calc_digits(row, include_words):
+    digits = [-1, -1]
 
-def main():
-    print(task_one())
+    nums = [str(x) for x in range(10)]
     
-    print(task_two())
+    if include_words:
+        nums.extend([num2words(x) for x in range(10)])
+
+    min_first_idx = np.inf
+    max_last_idx = -1
+
+    for num in nums:
+        first_idx = row.find(num)
+        last_idx = row.rfind(num)
+        
+        if first_idx < min_first_idx and first_idx != -1:
+            digits[0] = int(num) if row[first_idx].isdigit() else int(w2n(row[first_idx : first_idx + len(num)]))
+            min_first_idx = first_idx
+
+        if last_idx > max_last_idx:
+            digits[1] = int(num) if row[last_idx].isdigit() else int(w2n(row[last_idx : last_idx + len(num)]))
+            max_last_idx = last_idx
+
+    return digits 
+
+
+def main(is_task2):
+    input = get_input()
+
+    sum = 0
+
+    for row in input:
+        digits = calc_digits(row, is_task2)
+        sum += digits[0] * 10 + digits[1]
+
+    return sum
+
+print("Task 1: ", main(False))
+    
+print("Task 2: ", main(True))
--- template.py
+++ template.py
@@ -1,15 +1,31 @@
 import numpy as np
 import pandas as pd
+from datetime import date
 
-def task_one():
-    return ""
-
-
-def task_two():
-    return ""
+def get_day():
+    return date.today().strftime('%-d')
 
 
-def main():
-    print(task_one())
+def get_input():
+    with open(f'./day{get_day()}/input.txt', 'r') as input:
+        return input.readlines()
     
-    print(task_two())
+
+def get_testinput(task):
+    if task not in [1, 2]:
+        raise Exception('Not a valid task.')
+    else:
+        with open(f'./day{get_day()}/testinput{task}.txt', 'r') as input:
+            return input.readlines()
+
+
+def main(is_task2):
+    if not is_task2:
+        pass
+    else:
+        pass
+
+
+print("Task 1: ", main(False))
+
+print("Task 2: ", main(True))
