f4fce9dc4d021b5a1fb9809ec9fdda61fe14da02
Micha-Alexander Pries <micha-alexander.pries@rwth-aachen.de>
2023-12-04 09:41:21 +0100

    finished day 4

--- day4/script.py
+++ day4/script.py
@@ -20,28 +20,30 @@ def get_testinput(task):
             return input.readlines()
 
 
-def task_one():
+def main(is_task2):
     input = [x.strip('\n') for x in get_input()] 
     
     sum = 0
+    card_frequencies = [1] * len(input)
 
-    for row in input:
+    for idx, row in enumerate(input):
         split = [x.split(' ') for x in row.replace('  ', ' ').split(': ')[1].split(' | ')] 
         for i in range(len(split)):
             split[i] = [int(x) for x in split[i]]
         
         shared_elems = len(set(split[0]).intersection(split[1]))
-        
+
+        # Task 1
         sum += 2 ** (shared_elems - 1) if shared_elems > 0 else 0
 
-    return sum
+        # Task 2
+        for i in range(1, shared_elems+1):
+            card_frequencies[idx + i] += card_frequencies[idx]
 
-
-def main(is_task2):
     if not is_task2:
-        return task_one()
+        return sum
     else:
-        pass
+        return np.sum(card_frequencies)
 
 
 print("Task 1: ", main(False))
