59d27626df3aaa73b41c6e4e7e9026ebcdc38ff8
Micha-Alexander Pries <micha-alexander.pries@rwth-aachen.de>
2023-12-07 12:51:11 +0100

    completed day 7

--- day7/script.py
+++ day7/script.py
@@ -22,7 +22,7 @@ def get_testinput(task):
             return input.readlines()
 
 
-def part1():
+def main(is_part1):
     input = [x.strip('\n').split(' ') for x in get_testinput(1)]
     input = [x.strip('\n').split(' ') for x in get_input()]
     input = [(hand, int(bid)) for hand, bid in input]
@@ -30,11 +30,11 @@ def part1():
     hands_with_strength = defaultdict(list)
 
     for hand, bid in input:
-        hands_with_strength[get_strength(hand)].append((hand, bid))
+        hands_with_strength[get_strength(hand, is_part1)].append((hand, bid))
 
     for strength, hands_bids in hands_with_strength.items():
-        hands_bids = [([*hand], bid) for hand, bid in hands_bids]     # Converts string to char lists
-        hands_with_strength[strength] = sorted([(convert_hand_to_int(hand), bid) for hand, bid in hands_bids], reverse=True)
+        hands_bids = [([*hand], bid) for hand, bid in hands_bids]     # Converts string to char lists.
+        hands_with_strength[strength] = sorted([(convert_hand_to_int(hand, is_part1), bid) for hand, bid in hands_bids], reverse=True)
 
     sum = 0
     i = 0
@@ -47,25 +47,46 @@ def part1():
 
 
 # Returns hand as a list of ints based on card_values.
-def convert_hand_to_int(hand, is_task1):
-    card_values = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
-    card_values = {'A': 14, 'K': 13, 'Q': 12, 'T': 10, 'J': 1}
+def convert_hand_to_int(hand, is_part1):
+    converted_hand = hand.copy()
+    if is_part1:
+        card_values = {'A': 14, 'K': 13, 'Q': 12, 'J': 11, 'T': 10}
+    else:
+        card_values = {'A': 14, 'K': 13, 'Q': 12, 'T': 10, 'J': 1}
 
     for i in range(len(hand)):
         if hand[i] in card_values.keys():
-            hand[i] = card_values[hand[i]]
+            converted_hand[i] = card_values[hand[i]]
         else:
-            hand[i] = int(hand[i])
+            converted_hand[i] = int(hand[i])
     
-    return hand
+    return converted_hand
 
 
 # Ordering: Five of a kind: 5, four of a kind: 4, full house: 3.5, three of a kind: 3, two pair: 2, one pair: 1, high card: 0
-def get_strength(hand):
-    counts = dict(Counter(hand))
-    count_vals = counts.values()
-    highest_freq, lowest_freq = np.max(list(count_vals)), np.min(list(count_vals))
+def get_strength(hand, is_part1):
+    counts = dict(Counter(convert_hand_to_int([*hand], is_part1)))
+    
+    if not is_part1:
+        if 1 in counts.keys():
+            del counts[1]
 
+        # Most frequent cards in the hand.
+        max_count_keys = [count_key for count_key, count_val in counts.items() if count_val == max(counts.values())]
+        
+        # Best card of the most frequent ones.
+        if max_count_keys:
+            max_card = np.max([key for key in max_count_keys])
+        
+        # Iff hand = 'JJJJJ', counts and max_card are empty.
+        if counts and max_card:
+            counts[max_card] += len(hand) - np.sum(list(counts.values()))           # Raise frequency of best card in the hand by the number of jokers in the hand
+        else:
+            counts[1] = 5
+    
+    highest_freq, lowest_freq = np.max(list(counts.values())), np.min(list(counts.values()))
+    
+    # Actual logic for strength calculation.
     if highest_freq in [4, 5]:
         return highest_freq
     elif highest_freq == 3:
@@ -76,21 +97,10 @@ def get_strength(hand):
     elif highest_freq == 1:
         return 0
     else:
-        if len(count_vals) == 3:
+        if len(counts.values()) == 3:
             return 2
         else:
             return 1
-
-
-def part2():
-    return
-
-
-def main(is_part1):
-    if is_part1:
-        return part1()
-    else:
-        return part2()
 
 
 print("Part 1: ", main(True))
