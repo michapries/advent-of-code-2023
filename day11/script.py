88d9f59a0554f84156bf285671c75e6ea502724e
Micha-Alexander Pries <micha-alexander.pries@rwth-aachen.de>
2023-12-11 16:19:23 +0100

    completed day 11

--- day11/script.py
+++ day11/script.py
@@ -14,49 +14,44 @@ def get_input():
 
 def get_testinput(task):
     if task not in [1, 2]:
-        raise Exception('Not a valid task.')
+            raise Exception('Not a valid task.')
     else:
         with open(f'./day11/testinput{task}.txt', 'r') as input:
             return input.readlines()
 
 
-def part1():
+def main(is_part1):
     input = np.array([[*x.strip('\n')] for x in get_testinput(1)])
     input = np.array([[*x.strip('\n')] for x in get_input()])
+    
+    multiplicator = 1000000 if not is_part1 else 2
 
-    input = duplicate_empty_rows(input)
-    input = np.transpose(duplicate_empty_rows(np.transpose(input)))
+    horizontal_empties = list()
+    vertical_empties = list()
+    for idx in range(input.shape[1]):
+        if np.count_nonzero(input[idx] == '.') == input.shape[1]:
+            horizontal_empties.append(idx)
+    for idx in range(input.shape[0]):
+        if np.count_nonzero(input[:, idx] == '.') == input.shape[0]:
+            vertical_empties.append(idx)
     
     galaxies = np.transpose(np.where(input == '#'))
-
-    shortest_paths = np.full((len(galaxies), len(galaxies)), np.inf)
     
+    shortest_paths = np.full((len(galaxies), len(galaxies)), np.inf)
+
     for idx1, galaxy1 in enumerate(galaxies):
         for idx2, galaxy2 in enumerate(galaxies):
-            shortest_paths[idx1, idx2] = np.abs(galaxy1[0] - galaxy2[0]) + np.abs(galaxy1[1] - galaxy2[1])
+            vertical_empties_in_range = [x for x in vertical_empties if x in range(galaxy1[1], galaxy2[1]) or x in range(galaxy2[1], galaxy1[1])]
+            vertical_empty_count = len(vertical_empties_in_range)
+            horizontal_empties_in_range = [x for x in horizontal_empties if x in range(galaxy1[0], galaxy2[0]) or x in range(galaxy2[0], galaxy1[0])]
+            horizontal_empty_count = len(horizontal_empties_in_range)
+
+            cross_point_count = vertical_empty_count + horizontal_empty_count - len(set(vertical_empties_in_range).intersection(horizontal_empties_in_range))
+
+            shortest_paths[idx1, idx2] = np.abs(galaxy1[0] - galaxy2[0]) + np.abs(galaxy1[1] - galaxy2[1]) + horizontal_empty_count*multiplicator + vertical_empty_count*multiplicator - cross_point_count
 
     return int(np.sum(shortest_paths/2))
-
-
-def duplicate_empty_rows(input):
-    inserted = 0
-    for idx, row in enumerate(input):
-        if np.count_nonzero(row == '.') == input.shape[1]:
-            input = np.insert(input, idx+inserted, row, 0)
-            inserted += 1
-    return input
-
-
-def part2():
-    return
-
-
-def main(is_part1):
-    if is_part1:
-        return part1()
-    else:
-        return part2()
-
+    
 
 print("Part 1: ", main(True))
 print("Part 2: ", main(False))
